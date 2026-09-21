e4cffb59a87c4abbc92fc0b62877205a14b77881
Michal Mrowicki <mrg.mrowicki@gmail.com>
2026-09-21 20:46:08 +0200

    changing main loop to solve 'spaces at the beginning', 'only lowercase letters' scenarios

--- main.cpp
+++ main.cpp
@@ -7,14 +7,35 @@ using namespace std;
 int main(){
 
     char letters[256];
+    bool beginningAchieved = false;
     cout << "Enter a string:" << endl;
     cin.getline(letters, 256);
     
     cout <<"Initialism:"<<endl;
-    for(int i = 0; letters[i] != '\0'; i++){
-        if(letters[i] >= 'A' && letters[i] <= 'Z'){
-            cout << (char)toupper(letters[i]);
+    int i = 0;
+
+    //skip spaces at the beginning of the string
+    while(beginningAchieved == false && letters[i] == ' '){
+        i++;
+    }
+    beginningAchieved = true;    //we found the first char of the string
+    cout<< (char)toupper(letters[i]); // first char of the string is printed in uppercase
+
+    while(letters[i] != ' ' && letters[i] != '\0'){ 
+        i++; //skip the letters in the word
+        if(letters[i] == ' '){
+            i++; //skip the space
+            cout<< (char)toupper(letters[i]);
         }
     }
+
+    cout << endl;
+
     return 0;
 }
+
+/*
+
+  example input to test this program
+
+*/
