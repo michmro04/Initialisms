d1e1be820f1ffa7dc2c426dd0cf7acd4b6fade20
Michal Mrowicki <mrg.mrowicki@gmail.com>
2026-09-24 22:03:35 +0200

    Dodanie skryptu z testami automatycznymi

--- test_runner.cpp
+++ test_runner.cpp
@@ -5,7 +5,7 @@
 
 using namespace std;
 
-//funkcja do skrótów
+//funkcja do skrotow
 string createInitialism(const string& str){
     string result = "";
     bool newWord = true;
@@ -27,22 +27,39 @@ int failedCount = 0;
 
 void assertEqual(const string& testName, const string& expected, const string& actual){
     if(actual == expected){
-        cout << "[PASSED]" << testName << endl;
+        cout << "[PASSED] " << testName << endl;
         passedCount++;
     } else {
-        cout << "[FAILED]" << testName << endl;
+        cout << "[FAILED] " << testName << endl;
         cout << "Expected: " << expected << endl;
         cout << "Actual: " << actual << endl;
         failedCount++;
     }
 }
 
-//główna funkcja
+//glowna funkcja
 int main(){
+    cout << "Uruchomienie testow jednostkowych funkcji createInitialism" << endl;
 
+    //testy jednostkowe
+    assertEqual("Test 1: Same wielkie litery", "PTDTP", createInitialism("PRoBNY TEKST DO TESTOWANIA PROGRAMU"));
+    assertEqual("Test 2: Same male litery", "PTDTP", createInitialism("probny tekst do testowania programu"));
+    assertEqual("Test 3: Wielkie i male litery na poczatku i w srodku wyrazow", "PTDTP", createInitialism("ProBny teKsT Do teStoWAnia pRogRAmu"));
+    assertEqual("Test 4: Kilka spacji na poczatku", "PTDTP", createInitialism("   probny tekst do testowania programu"));
+    assertEqual("Test 5: Kilka spacji miedzy slowami", "PTDTP", createInitialism("probny           tekst     do  testowania   programu"));
+    assertEqual("Test 6: Spacje na poczatku i miedzy slowami", "PTDTP", createInitialism("    probny   tekst       do  testowania   programu"));
 
-
-
-
-    return 0;
+    //podsumowanie
+    cout << "==========================================================" << endl;
+    cout << "Wynik:" << endl;
+    cout << "Ilosc testow: " << passedCount + failedCount << endl;
+    cout << "Pomyslne: " << passedCount << endl;
+    cout << "Niepomyslne: " << failedCount << endl;
+    return (failedCount == 0) ? 0 : 1;
 }
+
+/*
+g++ test_runner.cpp -o test.exe
+
+
+*/
